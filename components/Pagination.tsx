148353f6934fc5c9522041f7697481d8c51b6c14
ryo4004 <kato@ryo4004.net>
2021-06-20 01:45:46 +0900

    ページネーションを表示する数を調整

--- components/Pagination.tsx
+++ components/Pagination.tsx
@@ -3,14 +3,32 @@ import { PER_PAGE, range } from '../pages/api/pagination'
 
 import styles from '../styles/Pagination.module.scss'
 
+const PREV_COUNT = 2
+const NEXT_COUNT = 2
+
 export const Pagination = ({ totalCount, activePage }: { totalCount: number; activePage: number }) => {
   return (
     <ul className={styles.pagination}>
-      {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => (
-        <li key={index} className={number === activePage ? styles.active : ''}>
-          <NextLink to={'/page/' + number}>{number}</NextLink>
+      {activePage > 1 && (
+        <li>
+          <NextLink to={'/page/' + (activePage - 1)}>back</NextLink>
         </li>
-      ))}
+      )}
+      {range(1, Math.ceil(totalCount / PER_PAGE)).map((number, index) => {
+        if (activePage > number + PREV_COUNT || activePage < number - NEXT_COUNT) {
+          return null
+        }
+        return (
+          <li key={index} className={number === activePage ? styles.active : ''}>
+            <NextLink to={'/page/' + number}>{number}</NextLink>
+          </li>
+        )
+      })}
+      {activePage < Math.ceil(totalCount / PER_PAGE) && (
+        <li>
+          <NextLink to={'/page/' + (activePage + 1)}>next</NextLink>
+        </li>
+      )}
     </ul>
   )
 }
